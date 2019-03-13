a77dbb956d2b77d019fc8115bb91267e613cc10d
Ola Fivelstad Smaaberg <olafsm@stud.ntnu.no>
2019-03-13 15:24:04 +0100

    Hei2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import system;
-
-
 public class Main {
 
     int d = 10;
-    public static void main(String[] args){
-        system.out.print("hei");
+    public static void main(String[] args)
+    {
+        System.out.print("hei2");
     }
 }
